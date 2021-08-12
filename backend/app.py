d5b278763cc45bea7bd2cad901b2500fc0b732bb
xinzehuang <44016281+xinzehuang@users.noreply.github.com>
2021-08-12 20:38:59 +0800

    Create app.py
    Empty Initial Flask app for MySQL on localhost:5001

---
+++ backend/app.py
@@ -0,0 +1,36 @@
+# -*- coding:UTF-8 -*-
+
+from flask import Flask, render_template, request, flash, redirect, url_for, make_response, Response, jsonify
+
+import os, time
+
+# mysql
+from flask_sqlalchemy import SQLAlchemy
+
+def get32():
+    return ''.join(random.sample(string.ascii_letters + string.digits, 32))
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+app.config["SECRET_KEY"] = '123456'
+
+# database
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Hxz960722@127.0.0.1/textblocks'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
+
+# db = SQLAlchemy()
+# db.app = app
+# db.init_app(app)
+
+
+@app.route('/')
+def index():
+    return 'hello'
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True, threaded=True, port=5001, host='127.0.0.1')
+
+
