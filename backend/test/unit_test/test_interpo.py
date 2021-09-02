23182929f620edffdb2ab1ad0f90d459eb2b78cb
YuanHungLin <viclin@linyuanhongde-MacBook-Pro.local>
2021-09-03 00:41:10 +1000

    modified the comments

--- backend/test/unit_test/test_interpo.py
+++ backend/test/unit_test/test_interpo.py
@@ -1,11 +1,13 @@
 # unit tests for interpolation
-import sys
-sys.path.append("../../utility")
+
 import math
+# TODO: improve the issue of import module
 
 from interpolation import interpolation
 
-beams = [
+
+
+BEAMS = [
     {"beam_id": "Filter1", "kvp": 60, "hvl_measured_al": 1.268},
     {"beam_id": "Filter2", "kvp": 80, "hvl_measured_al": 2.321},
     {"beam_id": "Filter3", "kvp": 100, "hvl_measured_al": 2.881},
@@ -15,22 +17,22 @@ beams = [
     {"beam_id": "Filter7", "kvp": 200, "hvl_measured_cu": 1.042},
     {"beam_id": "Filter8", "kvp": 250, "hvl_measured_cu": 2.117}
 ]
-ref_beams = [
+REF_BEAMS = [
     {"beam_farmer_id": "NXJ60", "kvp": 60, "hvl_measured_cu": None, "hvl_measured_al": 0.788813791651093, "nk_3587": 49.8986656857429, "nk_5447": 50.7, "nk_5448": 51.5},
     {"beam_farmer_id": "NXK60", "kvp": 60, "hvl_measured_cu": None, "hvl_measured_al": 1.28860110150315, "nk_3587": 48.3988130247751, "nk_5447": 49.3, "nk_5448": 50.1}
 ]
 
-ref_beams2=[
+REF_BEAMS2=[
     {"beam_farmer_id": "NXB120", "kvp": 120, "hvl_measured_cu": 0.229716813754473, "hvl_measured_al": 5.55599697591481, "nk_3587": 47.7093008745159, "nk_5447": 47.8597516857949, "nk_5448": 48.5210001193312},
     {"beam_farmer_id": "NXC120", "kvp": 120, "hvl_measured_cu": 0.280623113903494, "hvl_measured_al": 6.37697784783916, "nk_3587": 47.7411047431016, "nk_5447": 47.8406595597577, "nk_5448": 48.4969968164535}
 ]
 
-ref_beams3 = [
+REF_BEAMS3 = [
     {"beam_farmer_id": "NXE250", "kvp": 250, "hvl_measured_cu": 1.61280416689842, "hvl_measured_al": None, "nk_3587": 48.0926889791271, "nk_5447": 47.8694372925679, "nk_5448": 48.1739224296041},
     {"beam_farmer_id": "NXF250", "kvp": 250, "hvl_measured_cu": 2.13567218440762, "hvl_measured_al": None, "nk_3587": 48.115151223515, "nk_5447": 47.9021260939212, "nk_5448": 48.1429169731479}
 ]
 
-ref_beams4 =[
+REF_BEAMS4 =[
     {"beam_plainparallel_id": "RT5", "kvp": 55, "hvl_measured_al": 0.8060, "nk_1508": 1033.92690609361, "nk_858": 82.0878589140421},
     {"beam_plainparallel_id": "RT6", "kvp": 70, "hvl_measured_al": 1.3040, "nk_1508": 1025.75342949661, "nk_858": 81.7610639245557}
 ]
@@ -40,7 +42,7 @@ ref_beams4 =[
 ###########################
 # test case: [Farmer] only get hvl_al without hvl_cu (interpolation)
 def test_interpo1():
-    ans = interpolation(ref_beams[0]["nk_3587"], ref_beams[1]["nk_3587"], ref_beams[0]["hvl_measured_al"], ref_beams[1]["hvl_measured_al"], beams[0]["hvl_measured_al"])
+    ans = interpolation(REF_BEAMS[0]["nk_3587"], REF_BEAMS[1]["nk_3587"], REF_BEAMS[0]["hvl_measured_al"], REF_BEAMS[1]["hvl_measured_al"], BEAMS[0]["hvl_measured_al"])
 
     assert math.isclose(ans, 48.4606365571043)
     # assert round(ans,5) == round(48.4606365571043, 5)
@@ -48,33 +50,33 @@ def test_interpo1():
 
 # test case: [Farmer] only get hvl_al without hvl_cu (extrap)
 def test_interpo2():
-    ans = interpolation(ref_beams2[0]["nk_5447"], ref_beams2[1]["nk_5447"], ref_beams2[0]["hvl_measured_al"], ref_beams2[1]["hvl_measured_al"], beams[3]["hvl_measured_al"])
+    ans = interpolation(REF_BEAMS2[0]["nk_5447"], REF_BEAMS2[1]["nk_5447"], REF_BEAMS2[0]["hvl_measured_al"], REF_BEAMS2[1]["hvl_measured_al"], BEAMS[3]["hvl_measured_al"])
 
     assert math.isclose(ans, 47.8698211443177)
 
 # test case: [Farmer] only get hvl_cu without hvl_al (interpolation)
 def test_interpo3():
-    ans = interpolation(ref_beams3[0]["nk_3587"], ref_beams3[1]["nk_3587"], ref_beams3[0]["hvl_measured_cu"], ref_beams3[1]["hvl_measured_cu"], beams[-1]["hvl_measured_cu"])
+    ans = interpolation(REF_BEAMS3[0]["nk_3587"], REF_BEAMS3[1]["nk_3587"], REF_BEAMS3[0]["hvl_measured_cu"], REF_BEAMS3[1]["hvl_measured_cu"], BEAMS[-1]["hvl_measured_cu"])
 
     assert math.isclose(ans, 48.1143490723882)
 
 # test case: [Farmer] only get hvl_cu without hvl_al (extrap)
 def test_interpo4():
-    ans = interpolation(ref_beams2[0]["nk_5447"], ref_beams2[1]["nk_5447"], ref_beams2[0]["hvl_measured_cu"], ref_beams2[1]["hvl_measured_cu"], beams[3]["hvl_measured_cu"])
+    ans = interpolation(REF_BEAMS2[0]["nk_5447"], REF_BEAMS2[1]["nk_5447"], REF_BEAMS2[0]["hvl_measured_cu"], REF_BEAMS2[1]["hvl_measured_cu"], BEAMS[3]["hvl_measured_cu"])
 
     assert math.isclose(ans, 47.8607706117524)
 
 # test case: [Farmer] get both hvl_al and hvl_cu (Average nk value)
 def test_interpo5():
-    ans_al = interpolation(ref_beams2[0]["nk_5447"], ref_beams2[1]["nk_5447"], ref_beams2[0]["hvl_measured_al"], ref_beams2[1]["hvl_measured_al"], beams[3]["hvl_measured_al"])
-    ans_cu = interpolation(ref_beams2[0]["nk_5447"], ref_beams2[1]["nk_5447"], ref_beams2[0]["hvl_measured_cu"], ref_beams2[1]["hvl_measured_cu"], beams[3]["hvl_measured_cu"])
+    ans_al = interpolation(REF_BEAMS2[0]["nk_5447"], REF_BEAMS2[1]["nk_5447"], REF_BEAMS2[0]["hvl_measured_al"], REF_BEAMS2[1]["hvl_measured_al"], BEAMS[3]["hvl_measured_al"])
+    ans_cu = interpolation(REF_BEAMS2[0]["nk_5447"], REF_BEAMS2[1]["nk_5447"], REF_BEAMS2[0]["hvl_measured_cu"], REF_BEAMS2[1]["hvl_measured_cu"], BEAMS[3]["hvl_measured_cu"])
     ans = (ans_al+ans_cu)/2
 
     assert math.isclose(ans, 47.8652958780351)
 
 # test case: [Plain Parallel] interpolation
 def test_interpo6():
-    ans = interpolation(ref_beams4[0]["nk_1508"], ref_beams4[1]["nk_1508"], ref_beams4[0]["hvl_measured_al"], ref_beams4[1]["hvl_measured_al"], beams[0]["hvl_measured_al"])
+    ans = interpolation(REF_BEAMS4[0]["nk_1508"], REF_BEAMS4[1]["nk_1508"], REF_BEAMS4[0]["hvl_measured_al"], REF_BEAMS4[1]["hvl_measured_al"], BEAMS[0]["hvl_measured_al"])
 
     assert math.isclose(ans, 1026.34428322651)
 
--- backend/utility/nk_value.py
+++ backend/utility/nk_value.py
@@ -1,92 +1,221 @@
 from interpolation import interpolation
-# Interpolation/Extrapolation function
 
-#                        c     /    a
-#                             /                         a - target_val           c - d
-#	    					 /                         ---------------   =   ---------------
-# (target_known_val) d	    /    (target_val)               a - b                c - e
-#                          /
-#                         /
-#                 e      /     b
+import pyodbc
 
+# TODO: collect input data from DB?
 # input data from the `identification` sheet
-beams = [
-    {"beam_id": "Filter1", "kvp": 60, "hvl_measured_al": 1.268},
-    {"beam_id": "Filter2", "kvp": 80, "hvl_measured_al": 2.321},
-    {"beam_id": "Filter3", "kvp": 100, "hvl_measured_al": 2.881},
+BEAMS = [
+    {"beam_id": "Filter1", "kvp": 60, "hvl_measured_al": 1.268, "hvl_measured_cu": None},
+    {"beam_id": "Filter2", "kvp": 80, "hvl_measured_al": 2.321, "hvl_measured_cu": None},
+    {"beam_id": "Filter3", "kvp": 100, "hvl_measured_al": 2.881, "hvl_measured_cu": None},
     {"beam_id": "Filter4", "kvp": 120, "hvl_measured_al": 5.123, "hvl_measured_cu": 0.227},
-    {"beam_id": "Filter5", "kvp": 150, "hvl_measured_cu": 0.339},
-    {"beam_id": "Filter6", "kvp": 180, "hvl_measured_cu": 0.504},
-    {"beam_id": "Filter7", "kvp": 200, "hvl_measured_cu": 1.042},
-    {"beam_id": "Filter8", "kvp": 250, "hvl_measured_cu": 2.117}
+    {"beam_id": "Filter5", "kvp": 150, "hvl_measured_al": None, "hvl_measured_cu": 0.339},
+    {"beam_id": "Filter6", "kvp": 180, "hvl_measured_al": None, "hvl_measured_cu": 0.504},
+    {"beam_id": "Filter7", "kvp": 200, "hvl_measured_al": None, "hvl_measured_cu": 1.042},
+    {"beam_id": "Filter8", "kvp": 250, "hvl_measured_al": None, "hvl_measured_cu": 2.117}
 ]
 
+CHAMBER_SN_FARMER = ["3587", "5447", "5448"]
+CHAMBER_SN_PP = ["1508", "858"]
 
+'''
 ###########################
 #### Main Calculation #####
 ###########################
-def cal_nk_value(beams: list):
+'''
+def cal_nk_value(beams):
 
+    # connect to database
+    connection = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
+                          'Server=34.126.203.116,1433;'
+                          'Database=violet_main;'
+                          'Uid=SA;'
+                          'PWD=ProjViolet!1;'
+                          'Trusted_Connection=no;')
+    # Create cursor object
+    cursor = connection.cursor()
+
+    # Loop through each input beam
     for beam in beams:
-        # step 0.5
+        # Initialize result list
+        result_list = []
+
         # CHECK if it's Al or Cu or Both
-        hvl_type = checkType(beam)
+        HVL_type = check_HVL_Al_Cu(beam)
 
-        # step 1
-        # SELECT search info from DB (including their data e.g. measured HVL, Nk...)
-        # step 2
-        # given previous 2 points' nk values (a, b), and hvl values (c, e), and beam["hvl_measured_al(cu)"] (d) -> do interpolation
+        # DEBUG
+        # print(beam["beam_id"], beam)
 
-        # both Al and Cu exist, need to calculate average value
-        if hvl_type["Al"] and hvl_type["Cu"]:
-            ref_value_al = selectDbFarmer(beam["kvp"], beam["hvl_measured_al"])
-            ref_value_cu = selectDbFarmer(beam["kvp"], beam["hvl_measured_cu"])
-            # TODO: Average interpolation
-        
-        # otherwise
-        elif hvl_type["Al"]:
-            ref_value_al = selectDbFarmer(beam["kvp"], beam["hvl_measured_al"])
+        # Both HVL_Al and HVL_Cu exist
+        if HVL_type["Al"] and HVL_type["Cu"]:
+            first_beam_al, second_beam_al = select_from_farmer(cursor, beam["kvp"], beam["hvl_measured_al"], "al")
+            first_beam_cu, second_beam_cu = select_from_farmer(cursor, beam["kvp"], beam["hvl_measured_cu"], "cu")
+
+            for chamber in CHAMBER_SN_FARMER:
+                nk_al = interpolation(first_beam_al["nk_"+chamber], second_beam_al["nk_"+chamber],
+                                      first_beam_al["hvl_al"], second_beam_al["hvl_al"], beam["hvl_measured_al"])
+                nk_cu = interpolation(first_beam_cu["nk_"+chamber], second_beam_cu["nk_"+chamber],
+                                      first_beam_cu["hvl_cu"], second_beam_cu["hvl_cu"], beam["hvl_measured_cu"])
+                # Average the results
+                result = {"id": beam["beam_id"],
+                          "nk_"+chamber: (nk_al+nk_cu)/2
+                          }
+                result_list.append(result)
+
+        # Only HVL_Al
+        elif HVL_type["Al"]:
+            first_beam, second_beam = select_from_farmer(cursor, beam["kvp"], beam["hvl_measured_al"], "al")
+
+            for chamber in CHAMBER_SN_FARMER:
+                result = {"id": beam["beam_id"],
+                          "nk_"+chamber: interpolation(first_beam["nk_"+chamber], second_beam["nk_"+chamber],
+                                                       first_beam["hvl_al"], second_beam["hvl_al"],
+                                                       beam["hvl_measured_al"])
+                          }
+                result_list.append(result)
+
+        # Only HVL_Cu
+        elif HVL_type["Cu"]:
+            first_beam, second_beam = select_from_farmer(cursor, beam["kvp"], beam["hvl_measured_cu"], "cu")
+
+            for chamber in CHAMBER_SN_FARMER:
+                result = {"id": beam["beam_id"],
+                          "nk_"+chamber: interpolation(first_beam["nk_"+chamber], second_beam["nk_"+chamber],
+                                                       first_beam["hvl_cu"], second_beam["hvl_cu"],
+                                                       beam["hvl_measured_cu"])
+                          }
+                result_list.append(result)
+
+        # Check Plane-Parallel-Type
+        if HVL_type["Al"] and beam["hvl_measured_al"] <= 2.204:
+            first_beam, second_beam = selectDbPP(cursor, beam["kvp"], beam["hvl_measured_al"])
             # TODO: Interpolation
-        elif hvl_type["Cu"]:
-            ref_value_cu = selectDbFarmer(beam["kvp"], beam["hvl_measured_cu"])
-            # TODO: Interpolation
 
-        # also need to find Nk for Plane-Parallel-Type
-        if hvl_type["Al"] and beam["hvl_measured_al"]<=2.204:
-            ref_value_pp = selectDbPP(beam["kvp"], beam["hvl_measured_al"])
-            # TODO: Interpolation
-
-        # step 3
-        # INSERT ... store the nk value back into the database
+        # TODO: Insert results into database
         storeIntoDb()
-    
 
+        # DEBUG
+        # print(result_list)
+        # print("----------------------")
+
+
+'''
 ###########################
 #### Helper functions #####
 ###########################
+'''
 
-# check type of measured_hvl
-def checkType(info: dict) -> dict:
-    hvl_type = {"Al": False, "Cu": False}
-    if info["hvl_measured_al"]: hvl_type["Al"]=True
-    if info["hvl_measured_cu"]: hvl_type["Cu"]=True
+def check_HVL_Al_Cu(input):
+    HVL_type = {"Al": False, "Cu": False}
+    if input["hvl_measured_al"] is not None: HVL_type["Al"]=True
+    if input["hvl_measured_cu"] is not None: HVL_type["Cu"]=True
 
-    return hvl_type
+    return HVL_type
 
-# DB query: Look up farmer type chamber table
-def selectDbFarmer(kvp: int, hvl: float)->list:
-    # TODO: SELECT ... 
+# Select 2 closest beams from Farmer-Type-Chamber table
+def select_from_farmer(cursor, kvp, hvl, type):
 
-    return ["hvl_1", "hvl_2", "nk_1", "nk_2"]
+    # Kvp not be bound in the table
+    if not cursor.execute('SELECT * FROM beam_farmer_list WHERE kV={}'.format(kvp)).fetchall():
+        lower_kvp, = cursor.execute('SELECT TOP 1 kV FROM beam_farmer_list WHERE kV<={} ORDER BY kV DESC'.format(kvp)).fetchone()
+        upper_kvp, = cursor.execute('SELECT TOP 1 kV FROM beam_farmer_list WHERE kV>={} ORDER BY kV'.format(kvp)).fetchone()
+        first_lower_beam, second_lower_beam = select_from_farmer(cursor, lower_kvp, hvl, type)
+        first_upper_beam, second_upper_beam = select_from_farmer(cursor, upper_kvp, hvl, type)
+
+        lower_beam, upper_beam = {"id": first_lower_beam["id"]+"+"+first_upper_beam["id"]}, {"id": second_lower_beam["id"]+"+"+second_upper_beam["id"]}
+        for k, v in first_lower_beam.items():
+            if k != "id":
+                lower_beam[k] = interpolation(first_lower_beam[k], first_upper_beam[k], lower_kvp, upper_kvp, kvp)
+                upper_beam[k] = interpolation(second_lower_beam[k], second_upper_beam[k], lower_kvp, upper_kvp, kvp)
+                # lower_beam[k] = interpolation(first_lower_beam[k], second_lower_beam[k], first_lower_beam["hvl_cu"], second_lower_beam["hvl_cu"], hvl)
+                # upper_beam[k] = interpolation(first_upper_beam[k], second_upper_beam[k], first_upper_beam["hvl_cu"], second_upper_beam["hvl_cu"], hvl)
+
+        return lower_beam, upper_beam
+
+    lower_check = cursor.execute('SELECT * FROM beam_farmer_list WHERE hvl_measured_mm_{}<={} AND kV={}'.format(type,hvl,kvp)).fetchall()
+    upper_check = cursor.execute('SELECT * FROM beam_farmer_list WHERE hvl_measured_mm_{}>={} AND kV={}'.format(type,hvl,kvp)).fetchall()
+
+    # HVL in the scope (INTERPOLATION)
+    if lower_check != [] and upper_check != []:
+        lower_table = cursor.execute('SELECT beam_farmer_id, chamber_SN, nk_value FROM ' + 'beam_farmer_chamber ' +
+                                     'WHERE ' + 'beam_farmer_id ' + 'IN ' +
+                                     '( SELECT TOP 1 ' + 'beam_farmer_id ' +
+                                     'FROM ' + 'beam_farmer_list ' +
+                                     'WHERE ' + 'hvl_measured_mm_{}<={} AND kV={} '.format(type, hvl, kvp) +
+                                     'ORDER BY ' + 'hvl_measured_mm_{} '.format(type) + 'DESC)').fetchall()
+        upper_table = cursor.execute('SELECT beam_farmer_id, chamber_SN, nk_value FROM ' + 'beam_farmer_chamber ' +
+                                     'WHERE ' + 'beam_farmer_id ' + 'IN ' +
+                                     '( SELECT TOP 1 ' + 'beam_farmer_id ' +
+                                     'FROM ' + 'beam_farmer_list ' +
+                                     'WHERE ' + 'hvl_measured_mm_{}>={} AND kV={} '.format(type, hvl, kvp) +
+                                     'ORDER BY ' + 'hvl_measured_mm_{} '.format(type) + 'ASC)').fetchall()
+        lower_beam, upper_beam = {}, {}
+        # TODO: REFACTOR
+        # Extract NK from each Chamber_SN
+        for beam_id, chamber_SN, nk in lower_table:
+            lower_beam["id"] = beam_id
+            lower_beam["nk_" + chamber_SN] = nk
+        for beam_id, chamber_SN, nk in upper_table:
+            upper_beam["id"] = beam_id
+            upper_beam["nk_" + chamber_SN] = nk
+
+        # Extract HVL
+        (lower_hvl,) = cursor.execute(
+            "SELECT hvl_measured_mm_{} FROM beam_farmer_list WHERE beam_farmer_id='{}'".format(type, lower_beam[
+                "id"])).fetchone()
+        (upper_hvl,) = cursor.execute(
+            "SELECT hvl_measured_mm_{} FROM beam_farmer_list WHERE beam_farmer_id='{}'".format(type, upper_beam[
+                "id"])).fetchone()
+        lower_beam["hvl_" + type], upper_beam["hvl_" + type] = lower_hvl, upper_hvl
+
+        return lower_beam, upper_beam
+
+    # HVL not in the scope (EXTRAPOLATION)
+    first_beam, second_beam, extrap_table= {}, {}, []
+    if not lower_check:
+        extrap_table = cursor.execute('SELECT beam_farmer_id, chamber_SN, nk_value FROM '+'beam_farmer_chamber '+
+                                      'WHERE '+'beam_farmer_id '+'IN (SELECT TOP 2 beam_farmer_id '+
+                                      'FROM '+'beam_farmer_list '+'WHERE '+
+                                      'hvl_measured_mm_{}>={} AND kV={} '.format(type,hvl,kvp)+
+                                      'ORDER BY '+'hvl_measured_mm_{})'.format(type)).fetchall()
+        (first_beam["id"], _, _), (second_beam["id"], _, _) = extrap_table[0], extrap_table[1]
+    if not upper_check:
+        extrap_table = cursor.execute('SELECT beam_farmer_id, chamber_SN, nk_value FROM ' + 'beam_farmer_chamber ' +
+                                      'WHERE ' + 'beam_farmer_id ' + 'IN (SELECT TOP 2 beam_farmer_id ' +
+                                      'FROM ' + 'beam_farmer_list ' + 'WHERE ' +
+                                      'hvl_measured_mm_{}<={} AND kV={} '.format(type, hvl, kvp) +
+                                      'ORDER BY ' + 'hvl_measured_mm_{} DESC)'.format(type)).fetchall()
+
+        (first_beam["id"], _, _), (second_beam["id"], _, _) = extrap_table[1], extrap_table[0]
+
+    # Extract Nk from each Chamber_SN
+    for beam_id, chamber_SN, nk in extrap_table:
+        if beam_id==first_beam["id"]: first_beam["nk_"+chamber_SN] = nk
+        if beam_id==second_beam["id"]: second_beam["nk_"+chamber_SN] = nk
+
+    # Extract HVL
+    (first_hvl,) = cursor.execute(
+        "SELECT hvl_measured_mm_{} FROM beam_farmer_list WHERE beam_farmer_id='{}'".format(type, first_beam[
+            "id"])).fetchone()
+    (second_hvl,) = cursor.execute(
+        "SELECT hvl_measured_mm_{} FROM beam_farmer_list WHERE beam_farmer_id='{}'".format(type, second_beam[
+            "id"])).fetchone()
+    first_beam["hvl_" + type], second_beam["hvl_" + type] = first_hvl, second_hvl
+
+    return first_beam, second_beam
 
 # DB query: Look up plain parallel type table
-def selectDbPP(kvp: int, hvl: float)->list:
-    # TODO: SELECT ... 
+def selectDbPP(cursor, kvp, hvl, type="al"):
+    # TODO: SELECT ...
 
-    return ["hvl_1", "hvl_2", "nk_1", "nk_2"]
+    return "first", "second"
 
 # DB query: Store data into Db
 def storeIntoDb():
     #TODO: INSERT ...
-    
+
     return 0
+
+
+if __name__ == "__main__":
+    cal_nk_value(BEAMS)
