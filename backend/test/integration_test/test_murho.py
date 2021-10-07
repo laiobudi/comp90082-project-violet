6ca9add60d25aa662c9657e5fa3028ba7ce0791d
hlly1 <hlly13795@outlook.com>
2021-10-07 22:54:24 +0800

    add boundary tests, bou_test1 finished but not executed, weird

--- backend/test/integration_test/test_murho.py
+++ backend/test/integration_test/test_murho.py
@@ -10,44 +10,29 @@ Since correct data from Client's excel files are less,
 it is easy to test them all, just do it first.
 Then boundary test will be applied.
 """
-beams_hogwarts = [
-    {"beam_id": "Filter1", "kvp": 60, "hvl_measured_al": 1.268, "hvl_measured_cu": None},
-    {"beam_id": "Filter2", "kvp": 80, "hvl_measured_al": 2.321, "hvl_measured_cu": None},
-    {"beam_id": "Filter3", "kvp": 100, "hvl_measured_al": 2.881, "hvl_measured_cu": None},
-    {"beam_id": "Filter4", "kvp": 120, "hvl_measured_al": 5.123, "hvl_measured_cu": 0.227},
-    {"beam_id": "Filter5", "kvp": 150, "hvl_measured_al": None, "hvl_measured_cu": 0.339},
-    {"beam_id": "Filter6", "kvp": 180, "hvl_measured_al": None, "hvl_measured_cu": 0.504},
-    {"beam_id": "Filter7", "kvp": 200, "hvl_measured_al": None, "hvl_measured_cu": 1.042},
-    {"beam_id": "Filter8", "kvp": 250, "hvl_measured_al": None, "hvl_measured_cu": 2.117}
-]
-beams_hogwarts_excel = [
-    {"al_murho": 1.018, "cu_murho": None, "murho": 1.018},
-    {"al_murho": 1.019, "cu_murho": None, "murho": 1.019},
-    {"al_murho": 1.021, "cu_murho": None, "murho": 1.021},
-    {"al_murho": 1.030, "cu_murho": 1.030, "murho": 1.030},
-    {"al_murho": None, "cu_murho": 1.038, "murho": 1.038},
-    {"al_murho": None, "cu_murho": 1.050, "murho": 1.050},
-    {"al_murho": None, "cu_murho": 1.077, "murho": 1.077},
-    {"al_murho": None, "cu_murho": 1.090, "murho": 1.090}
-]
-beams_springfield = [
-    {"beam_id": "F1", "kvp": 30, "hvl_measured_al": 0.190, "hvl_measured_cu": None},
-    {"beam_id": "F2", "kvp": 50, "hvl_measured_al": 0.810, "hvl_measured_cu": None},
-    {"beam_id": "F3", "kvp": 80, "hvl_measured_al": 2.010, "hvl_measured_cu": None},
-    {"beam_id": "F4", "kvp": 95, "hvl_measured_al": 2.610, "hvl_measured_cu": None},
-    {"beam_id": "F5", "kvp": 100, "hvl_measured_al": 4.020, "hvl_measured_cu": None}
-]
-beams_springfield_excel = [
-    {"al_murho": 1.039, "cu_murho": None, "murho": 1.039},
-    {"al_murho": 1.022, "cu_murho": None, "murho": 1.022},
-    {"al_murho": 1.018, "cu_murho": None, "murho": 1.018},
-    {"al_murho": 1.020, "cu_murho": None, "murho": 1.020},
-    {"al_murho": 1.025, "cu_murho": None, "murho": 1.025}
-]
 
 
 @pytest.mark.parametrize("beams_hogwarts, beams_hogwarts_excel", [
-    (beams_hogwarts, beams_hogwarts_excel)
+    ([
+         {"beam_id": "Filter1", "kvp": 60, "hvl_measured_al": 1.268, "hvl_measured_cu": None},
+         {"beam_id": "Filter2", "kvp": 80, "hvl_measured_al": 2.321, "hvl_measured_cu": None},
+         {"beam_id": "Filter3", "kvp": 100, "hvl_measured_al": 2.881, "hvl_measured_cu": None},
+         {"beam_id": "Filter4", "kvp": 120, "hvl_measured_al": 5.123, "hvl_measured_cu": 0.227},
+         {"beam_id": "Filter5", "kvp": 150, "hvl_measured_al": None, "hvl_measured_cu": 0.339},
+         {"beam_id": "Filter6", "kvp": 180, "hvl_measured_al": None, "hvl_measured_cu": 0.504},
+         {"beam_id": "Filter7", "kvp": 200, "hvl_measured_al": None, "hvl_measured_cu": 1.042},
+         {"beam_id": "Filter8", "kvp": 250, "hvl_measured_al": None, "hvl_measured_cu": 2.117}
+     ],
+     [
+         {"al_murho": 1.018, "cu_murho": None, "murho": 1.018},
+         {"al_murho": 1.019, "cu_murho": None, "murho": 1.019},
+         {"al_murho": 1.021, "cu_murho": None, "murho": 1.021},
+         {"al_murho": 1.030, "cu_murho": 1.030, "murho": 1.030},
+         {"al_murho": None, "cu_murho": 1.038, "murho": 1.038},
+         {"al_murho": None, "cu_murho": 1.050, "murho": 1.050},
+         {"al_murho": None, "cu_murho": 1.077, "murho": 1.077},
+         {"al_murho": None, "cu_murho": 1.090, "murho": 1.090}
+     ])
 ])
 def test_hogwarts(beams_hogwarts, beams_hogwarts_excel):
     res_beams = add_murho(beams_hogwarts)
@@ -66,7 +51,20 @@ def test_hogwarts(beams_hogwarts, beams_hogwarts_excel):
 
 
 @pytest.mark.parametrize("beams_springfield, beams_springfield_excel", [
-    (beams_springfield, beams_springfield_excel)
+    ([
+         {"beam_id": "F1", "kvp": 30, "hvl_measured_al": 0.190, "hvl_measured_cu": None},
+         {"beam_id": "F2", "kvp": 50, "hvl_measured_al": 0.810, "hvl_measured_cu": None},
+         {"beam_id": "F3", "kvp": 80, "hvl_measured_al": 2.010, "hvl_measured_cu": None},
+         {"beam_id": "F4", "kvp": 95, "hvl_measured_al": 2.610, "hvl_measured_cu": None},
+         {"beam_id": "F5", "kvp": 100, "hvl_measured_al": 4.020, "hvl_measured_cu": None}
+     ],
+     [
+         {"al_murho": 1.039, "cu_murho": None, "murho": 1.039},
+         {"al_murho": 1.022, "cu_murho": None, "murho": 1.022},
+         {"al_murho": 1.018, "cu_murho": None, "murho": 1.018},
+         {"al_murho": 1.020, "cu_murho": None, "murho": 1.020},
+         {"al_murho": 1.025, "cu_murho": None, "murho": 1.025}
+     ])
 ])
 def test_springfield(beams_springfield, beams_springfield_excel):
     res_beams = add_murho(beams_springfield)
@@ -84,5 +82,23 @@ def test_springfield(beams_springfield, beams_springfield_excel):
         assert round(res_beams[i]["murho"], 3) == beams_springfield_excel[i]["murho"]
 
 
-test_hogwarts(beams_hogwarts, beams_hogwarts_excel)
-test_springfield(beams_springfield, beams_springfield_excel)
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+Boundary Tests:
+boundaries:
+0.03 <= First HVL(mm Al) <= 8.0
+0.1 <= First HVL(mm Cu) <= 5.0
+#IMPORTANT#: The expected results of boundary tests are calculated by
+Client's algorithm from excel files.
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+
+# First HVL(mm Al) == 0.3
+@pytest.mark.parametrize("test_input_beams,expected_result",
+                         [(
+                                 {"hvl_measured_al": 0.03, "hvl_measured_cu": None},
+                                 1.047
+                         )
+                         ])
+def boundary_test01_al(test_input_beams, expected_result):
+    res = add_murho(test_input_beams)
+    assert round(res["murho"], 3) == expected_result
