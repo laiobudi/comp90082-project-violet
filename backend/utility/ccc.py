7403b30d33f6d80cbafcb7ab2e57fd8615e37d5e
RuifengL <ruifengl3@student.unimelb.edu.au>
2021-10-12 11:29:49 +1100

    Added Exception to CCC

--- backend/utility/ccc.py
+++ backend/utility/ccc.py
@@ -6,7 +6,7 @@ Main function for calculating NK
 @:param cones: dictionary of cone information retrieved from db
 @:param beams: dictionary of beam data retrieved from db
 
-@:return results: list of dictionaries containing beam_id. cone_id, and k_closed_cone value
+@:return results: nested dictionaries containing beam_id. cone_id, and k_closed_cone value
 '''
 def cal_ccc_value(bw_res, cones, beams):
 
@@ -20,7 +20,13 @@ def cal_ccc_value(bw_res, cones, beams):
                 results[beam_cone_id] = result
 
             elif cone["open"] == "Closed":
-                bw_open = bw_res[beam_cone_id]["Bw_Combined"]
+                try:
+                    bw_open = bw_res[beam_cone_id]["Bw_Combined"]
+                except:
+                    # Or make k_closed_cone = 1?
+                    raise Exception('Beam and Cone ID pair does not exist in Bw_res. '
+                                    'Affected Beam ID = %s; Cone ID = %s' % (beam["beam_id"],
+                                                                             cone["cone_id"]))
                 result = {"k_closed_cone": cal_k_closed_cone(bw_open)}
                 results[beam_cone_id] = result
     return results
